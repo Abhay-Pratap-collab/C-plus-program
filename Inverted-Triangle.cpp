075720b25316dbaeeaf020bbd6b82c26015bdaab
Abhay Pratap Singh <at558449@gmail.com>
2025-08-23 15:55:30 +0530

    Update Inverted-Triangle.cpp

---
+++ Inverted-Triangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=0;
+
+for(int i=1;i<=4;i++)
+{
+	for(int j=i;j<=i+a;j++)
+	{
+	
+		cout<<j<<",";
+			
+	}
+a++;		
+	cout<<endl;
+}
+}
