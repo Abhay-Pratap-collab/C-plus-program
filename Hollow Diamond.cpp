656fe796363e540e890032eef8bbf13b5d1cb34a
Abhay Pratap Singh <at558449@gmail.com>
2025-08-24 10:23:24 +0530

    Add files via upload

---
+++ Hollow Diamond.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=2,b=3;
+	for(int i=0;i<4;i++)
+	{
+		for(int s=i;s<5;s++)
+		{
+			cout<<" ";
+		}
+		
+		
+			cout<<"*";
+		
+			for(int s=2;s<a;s++)
+		{
+			
+			
+			cout<<" ";
+		
+		}
+		a+=2;
+			for(int j=1;j<=1;j++)
+		{
+			if(i!=0)
+			{
+				
+			
+		
+			cout<<"*";
+		}
+		}
+		
+		cout<<endl;
+	}
+	for(int i=3;i<6;i++)
+	{
+			for(int s=0;s<i;s++)
+		{
+			cout<<" ";
+		}
+			for(int j=1;j<=1;j++)
+		{
+			cout<<"*";
+		}
+		for(int s=8;s>b;s--)
+		{
+			cout<<" ";
+		}
+		b+=2;
+			for(int j=1;j<=1;j++)
+		{
+			if(i!=5)
+			{
+			
+			cout<<"*";
+		}
+		}
+		
+		
+		cout<<endl;
+	}
+	
+}
+
